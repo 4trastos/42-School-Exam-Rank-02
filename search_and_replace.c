f2330d3e429fd795520b5e0deaad7d5b0d7a2904
David Gallego Gomez <davgalle@c1r12s1.42madrid.com>
2024-02-24 22:10:13 +0100

    ok

--- search_and_replace.c
+++ search_and_replace.c
@@ -1,42 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   search_and_replace.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davgalle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/24 20:51:45 by davgalle          #+#    #+#             */
+/*   Updated: 2024/02/24 21:53:28 by davgalle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	search_and_replace(char *str, char find, char change)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == find)
-			str[i] = change;
-		i++;
-	}
-	write(1, str, ft_strlen(str));
-}
 
 int	main(int argc, char **argv)
 {
-	if (argc == 4 && ft_strlen(argv[2]) == 1 && ft_strlen(argv[3]) == 1)
-		search_and_replace(argv[1], argv[2][0], argv[3][0]);
-	else
-		ft_putchar('\n');
+	int		i;
+
+	if (argc == 4  && !argv[2][1] && !argv[3][1])
+	{
+		i = 0;
+		while(argv[1][i] != '\0')
+		{
+			if (argv[1][i] == argv[2][0])
+				argv[1][i] = argv[3][0];
+			write(1, &argv[1][i], 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
 	return (0);
 }
