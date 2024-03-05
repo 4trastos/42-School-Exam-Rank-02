64fd8e238cecbefaa458684211d384e092d08316
4trastos <93524280+4trastos@users.noreply.github.com>
2024-03-05 11:42:52 +0100

    Add files via upload

---
+++ Level 2/ft_atoi.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int	ft_atoi(char *str)
+{
+	int result = 0;
+	int sign = 1;
+
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+        	str++;
+	if (*str == '-')
+		sign = -1;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + *str - '0';
+		str++;
+	}
+	return (sign * result);
+}
