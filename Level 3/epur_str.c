69cf338261b6a9825f70a09dbcb979d01622ca4e
4trastos <93524280+4trastos@users.noreply.github.com>
2024-03-06 19:57:42 +0100

    Add files via upload

--- Level 3/epur_str.c
+++ Level 3/epur_str.c
@@ -1,34 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   epur_str.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davgalle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/06 17:42:28 by davgalle          #+#    #+#             */
+/*   Updated: 2024/03/06 19:57:17 by davgalle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	epur_str(char *s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] != ' ' && s[i + 1] != ' ')
-			ft_putchar(s[i]);
-		if (s[i] != ' ' && s[i + 1] == ' ' && s[i + 2] != '\0')
-		{
-			ft_putchar(s[i]);
-			ft_putchar(' ');
-		}
-		if (s[i] != ' ' && s[i + 1] == ' ' && s[i + 2] == '\0')
-			ft_putchar(s[i]);
-		i++;
-	}
-}
+#include <stdlib.h>
+#include <stdio.h>
 
 int	main(int argc, char **argv)
 {
+	int	i;
+	int c;
+
 	if (argc == 2)
-		epur_str(argv[1]);
-	ft_putchar('\n');
+	{
+		i = 0;
+		c = 1;
+		while (argv[1][i] == ' ')
+			i++;
+		while (argv[1][i] != '\0')
+		{
+			if (argv[1][i] == ' ' && c == 0 && argv[1][i + 1] != '\0')
+			{
+				write(1, &argv[1][i], 1);
+				c = 1;
+				i++;
+			}
+			if (argv[1][i] != ' ')
+			{
+				write(1, &argv[1][i], 1);
+				c = 0;
+			}
+			i++;
+		}
+
+	}
+	write(1, "\n", 1);
 	return (0);
 }
