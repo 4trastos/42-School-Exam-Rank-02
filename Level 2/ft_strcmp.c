2e4ce523457523f93b963c2f5e2137e67f3d7da4
4trastos <93524280+4trastos@users.noreply.github.com>
2024-03-07 18:31:46 +0100

    Add files via upload

--- Level 2/ft_strcmp.c
+++ Level 2/ft_strcmp.c
@@ -1,17 +1,30 @@
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davgalle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/07 18:21:03 by davgalle          #+#    #+#             */
+/*   Updated: 2024/03/07 18:31:00 by davgalle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int    ft_strcmp(char *s1, char *s2)
 {
-	int	i;
+	int 	i;
 
 	i = 0;
-	while (s1[i] != '\0' || s2[i] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
 		i++;
 	}
-	return (0);
+	return (s1[i] + s2[i]);
 }
