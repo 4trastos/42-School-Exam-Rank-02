0c22e71446fd759ba77e753e25f008f3b54dc20b
4trastos <93524280+4trastos@users.noreply.github.com>
2024-03-07 18:20:01 +0100

    Add files via upload

--- Level 1/first_word.c
+++ Level 1/first_word.c
@@ -1,25 +1,48 @@
-#include <string.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   first_word.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davgalle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/07 18:06:34 by davgalle          #+#    #+#             */
+/*   Updated: 2024/03/07 18:18:43 by davgalle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-int	ft_isspace(int i)
+int	main(int argc, char **argv)
 {
-	if (i == '\t' || i == '\n' || i == '\v' || i == '\f' || i == ' ')
-		return (1);
-	return (0);
-}
+	int	i;
+	int	len;
+	int	z;
+	char *str;
 
-int	main(int argc, char *argv[])
-{
-	int		i;
-
-	i = 0;
 	if (argc == 2)
 	{
-		while (ft_isspace((argv[1][i])))
-			i += 1;
-		while (!(ft_isspace(argv[1][i])) && argv[1][i])
-			write(1, &argv[1][i++], 1);
+		i = 0;
+		len = 0;
+		z = 0;
+		str = argv[1];
+		while (str[z] != '\0')
+			z++;
+		while (str[len] == ' ')
+			len++;
+		if (len == z)
+		{
+			write(1, "\n", 1);
+			return (0);
+		}
+		while (str[i] == ' ')
+			i++;
+		while (str[i] != ' ' && str[i] != '\0')
+		{
+			write(1, &str[i], 1);
+			i++;
+		}
 	}
 	write(1, "\n", 1);
 	return (0);
