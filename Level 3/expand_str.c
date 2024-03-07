18e2fa031cb0b80b54c39ee332bd75feee0e01f1
4trastos <93524280+4trastos@users.noreply.github.com>
2024-03-07 12:54:38 +0100

    Add files via upload

---
+++ Level 3/expand_str.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expand_str.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davgalle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/07 11:57:12 by davgalle          #+#    #+#             */
+/*   Updated: 2024/03/07 12:52:55 by davgalle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char const **argv)
+{
+    int i;
+    int flag;
+
+    if (argc == 2)
+    {
+        i = 0;
+        while (argv[1][i] == ' ' || argv[1][i] == '\t')
+            i++;
+        while (argv[1][i])
+        {
+            if (argv[1][i] == ' ' || argv[1][i] == '\t')
+                flag = 1;
+            if (!(argv[1][i] == ' ' || argv[1][i] == '\t'))
+            {
+                if (flag)
+                    write(1, "   ", 3);
+                flag = 0;
+                write(1, &argv[1][i], 1);
+            }
+            i++;
+        }
+    }
+    write(1, "\n", 1);
+    return (0);
+}
