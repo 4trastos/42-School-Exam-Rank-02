d0510362d7f24b1a2452899bb9af14e613f5183c
Nicolas Gonzalez de Mendoza <73661812+ErMichoss@users.noreply.github.com>
2024-03-15 15:34:23 +0100

    Update ft_rrange.c

--- Level 3/ft_rrange/ft_rrange.c
+++ Level 3/ft_rrange/ft_rrange.c
@@ -10,31 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdlib.h>
-
-int *ft_rrange(int start, int end)
+int     *ft_rrange(int start, int end)
 {
-	int *range;
 	int i = 0;
-	int step = 1;
-	int n = end - start;
-
-	if (n < 0)
-		(n *= -1);
-	n++;
-
-	range = (int *)malloc(sizeof(int) * n);
-	if (range)
+	int *array;
+	array = (int *)malloc(sizeof(int) * (end - start));
+	while (end >= start)
 	{
-		if (start < end)
-			step = -1;
-		while (i < n)
-		{
-			range[i] = end;
-			end = end + step;
-			i++;
-		}
+		array[i] = end;
+		end--;
+		i++;
 	}
-	return (range);
+	return (array);
 }
