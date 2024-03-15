6be406cd22194f1f309c919488e85085b9f666b7
4trastos <93524280+4trastos@users.noreply.github.com>
2024-03-15 17:02:43 +0100

    Update inter.c

--- Level 2/inter/inter.c
+++ Level 2/inter/inter.c
@@ -13,30 +13,62 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int	iter(char *str, char c, int len)
+int	ft_repeat(char *str, char c, int numb)
 {
-	int	i = 0;
+	int i;
+	int count = 0;;
+	i = 0;
 
-	while (str[i] && (i < len || len == -1))
-		if (str[i++] == c)
-			return (1);
-	return (0);
+	while (str[i] != '\0' && i < numb)
+	{
+		if (str[i] == c)
+			count++;
+		i++;
+	}
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == c && i <= numb && count == 0)
+		   return (1);
+		if (str[i] == c && i >= numb && count != 0)
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-int	main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int	i;
+	int i;
+	int j;
+	int flag;
+	char *str;
+	char *str2;
 
 	if (argc == 3)
 	{
 		i = 0;
-		while (argv[1][i])
+		flag = 0;
+		str = argv[1];
+		str2 = argv[2];
+		while (str[i] != '\0')
 		{
-			if (!iter(argv[1], argv[1][i], i) && iter(argv[2], argv[1][i], -1))
-				write(1, &argv[1][i], 1);
-			i += 1;
+			j = 0;
+			while (str2[j] != '\0')
+			{
+				if (str2[j] == str[i])
+				{
+					if (ft_repeat(str, str[i], i) == 1)
+					{
+						write(1, &str[i], 1);
+						break;
+					}
+				}
+				j++;
+			}
+			i++;
 		}
 	}
-	write(1, "\n", 1);
+	write(1 , "\n", 1);
 	return (0);
 }
