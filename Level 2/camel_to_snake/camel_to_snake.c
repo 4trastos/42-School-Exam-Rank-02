138e717406f8f261bc864bda2a9fac21841c6ab4
4trastos <93524280+4trastos@users.noreply.github.com>
2024-03-18 11:08:39 +0100

    Update camel_to_snake.c

--- Level 2/camel_to_snake/camel_to_snake.c
+++ Level 2/camel_to_snake/camel_to_snake.c
@@ -4,23 +4,21 @@
 
 int main(int argc, char **argv)
 {
-	int	i;
-	char	*str;
+	int i;
+	char *str;
+
 	if (argc == 2)
 	{
 		i = 0;
 		str = argv[1];
-		if (str[i] >= 'a' && str[i] <= 'z')
-		{
-			write(1, &str[i], 1);
-			i++;
-		}
 		while (str[i] != '\0')
 		{
-			if (str[i] >= 'A' && str[i] <= 'Z')
-			{	
+			if (str[i] >= 'A' && str[i] <= 'Z' && i == 0)
 				str[i] = str[i] + 32;
+			else if (str[i] >= 'A' && str[i] <= 'Z' && i != 0)
+			{
 				write(1, "_", 1);
+				str[i] = str[i] + 32;
 			}
 			write(1, &str[i], 1);
 			i++;
