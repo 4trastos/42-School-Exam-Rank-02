a26d5c4d7fe2fe1fa5192d670015557fbe9535c2
Nicolas Gonzalez de Mendoza <73661812+ErMichoss@users.noreply.github.com>
2024-03-18 15:20:24 +0100

    Update rostring.c

--- Level 4/rostring/rostring.c
+++ Level 4/rostring/rostring.c
@@ -16,40 +16,34 @@
 
 int main(int argc, char **argv)
 {
-    int i;
-    int start;
-    int end;
-    int flag;
-
-    flag = 0;
-    if (argc > 1 && argv[1][0])
-    {
-        i = 0;
-        while (argv[1][i] == ' ' || argv[1][i] == '\t')
-            i++;
-        start = i;
-        while (argv[1][i] != '\0' && argv[1][i] != ' ' && argv[1][i] != '\t')
-            i++;
-        end = i;
-        while (argv[1][i] == ' ' || argv[1][i] == '\t')
-            i++;
-        while(argv[1][i]) 
-        {
-            while ((argv[1][i] == ' ' && argv[1][i + 1] == ' ') || (argv[1][i] == '\t' && argv[1][i + 1] == '\t'))
-                i++; 
-            if (argv[1][i] == ' ' || argv[1][i] == '\t')
-                flag = 1; 
-            write(1, &argv[1][i], 1);
-            i++;
-        }
-        if (flag)
-            write(1, " ", 1);
-        while (start < end)
-        {
-            write(1, &argv[1][start], 1);
-            start++;
-        }
-    }
-    write(1, "\n", 1);
-    return(0);
+	int i;
+	int start;
+	int end;
+	char *str = argv[1];
+	int j = 0;
+	while (str[j] != '\0')
+		j++;
+	while (str[i] == ' ' || str[i] == '\t')
+		i++;
+	start = i;
+	while (str[i] != '\0' && str[i] != ' ' && str[i] != '\t')
+		i++;
+	end = i;
+	while ((str[i] == ' ' || str[i] == '\t') && str[i] != '\0')
+		i++;
+	while(str[i] != '\0')
+	{
+		while ((str[i] == ' ' && str[i - 1] == ' ') || (str[i] == '\t' && str[i - 1] == '\t'))
+			i++;
+		if (i < j)
+			write(1, &str[i], 1);
+		i++;
+	}
+	while(start < end)
+	{
+		write(1, &str[start], 1);
+		start++;
+	}
+	write(1, "\n", 1);
+	return(0);
 }
