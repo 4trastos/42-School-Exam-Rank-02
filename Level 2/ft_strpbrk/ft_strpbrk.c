7ae14bf0b55a97e5c3b8e0ef97cce3903b43504f
4trastos <93524280+4trastos@users.noreply.github.com>
2024-03-18 15:53:48 +0100

    Update ft_strpbrk.c

--- Level 2/ft_strpbrk/ft_strpbrk.c
+++ Level 2/ft_strpbrk/ft_strpbrk.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 #include <unitd.h>
 #include <stdlib.h>
-#include <string.h>
 
 char	*ft_strpbrk(const char *s1, const char *s2)
 {
-	int	i;
+	int i = 0;
+	int j;
 
-	if (!s1 || !s2)
-		return (0);
-	while (*s1)
+	while (s1[i] != '\0')
 	{
-		i = 0;
-		while (s2[i] != '\0')
+		j = 0;
+		while (s2[j] != '\0')
 		{
-			if (s2[i] == *s1)
-				return ((char *)s1);
-			i++;
+			if (s2[j] == s1[i])
+				return ((char *)&s1[i]);
+			j++;
 		}
-		s1++;
+		i++;
 	}
 	return (0);
 }
