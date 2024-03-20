88ab4895be67b0ef95a8502c792628640a6f3c2d
4trastos <93524280+4trastos@users.noreply.github.com>
2024-03-20 11:25:06 +0100

    Update rstr_capitalizer.c

--- Level 3/rstr_capitalizer/rstr_capitalizer.c
+++ Level 3/rstr_capitalizer/rstr_capitalizer.c
@@ -14,36 +14,32 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-void    rstr_capitalizer(char *str)
-{
-    int i = 0;
-
-    while (str[i])
-    {
-        if (str[i] >= 'A' && str[i] <= 'Z')
-            str[i] = str[i] + 32;
-		else if ((str[i] >= 'a' && str[i] <= 'z') && (str[i + 1] == ' ' || str[i + 1] == '\t' || str[i + 1] == '\0'))
-            str[i] = str[i] -  32;
-        write(1, &str[i], 1);
-		i++;
-    }
-}
-
 int main(int argc, char **argv)
 {
-    int i;
+	int i;
+	int j;
 
-    if (argc == 1)
-        write(1, "\n", 1);
-    else
-    {
-        i = 1;
-        while (i < argc)
-        {
-            rstr_capitalizer(argv[i]);
-            write(1, "\n", 1);
-            i++;
-        }
-    }
-    return (0);
+	if (argc == 1)
+		write(1, "\n", 1);
+	else
+	{
+		i = 1;
+		while (argv[i])
+		{
+			j = 0;
+			while (argv[i][j] != '\0')
+			{
+				if ((argv[i][j] >= 'a' && argv[i][j] <= 'z') && (argv[i][j + 1] == ' ' || argv[i][j + 1] == '\0'))
+					argv[i][j] = argv[i][j] - 32;
+				else if ((argv[i][j] >= 'A' && argv[i][j] <= 'Z') && !(argv[i][j + 1] == ' ' || argv[i][j + 1] == '\0'))
+					argv[i][j] = argv[i][j] + 32;
+				write(1, &argv[i][j], 1);
+				j++;
+
+			}
+			write(1, "\n", 1);
+			i++;
+		}
+	}
+	return (0);
 }
