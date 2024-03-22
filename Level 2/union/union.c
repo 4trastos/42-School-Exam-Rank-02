0382fc0bc27ab582fd64d66355e37834fd3e350d
4trastos <93524280+4trastos@users.noreply.github.com>
2024-03-22 19:21:29 +0100

    Update union.c

--- Level 2/union/union.c
+++ Level 2/union/union.c
@@ -14,44 +14,60 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int check(int c, char *str, int index)
+int repeat(char *str, char c, int posit)
 {
 	int i = 0;
-	
-	while(i < index)
+	while (str[i] != '\0')
 	{
-		if (str[i] == c)
-			return 0;
+		if (str[i] == c && i == posit)
+			return (0);
+		else if (str[i] == c && i != posit)
+			return (1);
 		i++;
 	}
-	return  1;
+	return (0);
+}
+
+int	stayone(char *str, char c)
+{
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
 int main(int argc, char **argv)
-{	
-	int i = 0;
-	int j = 0;
-	int k = 0;
-	char *str = argv[1];
-	char *s2 = argv[2];
-	
+{
+	int i;
+	int j;
+	char *str;
+	char *s2;
 	if (argc == 3)
 	{
-		while(str[i] != '\0')
-			i++;
-		while(s2[j] != '\0')
+		i = 0;
+		str = argv[1];
+		s2 = argv[2];
+		while (str[i] != '\0')
 		{
-			str[i] = s2[j];
+			if (repeat(str, str[i], i) == 0)
+				write(1, &str[i], 1);
 			i++;
+		}
+		j = 0;
+		while (s2[j] != '\0')
+		{
+			if (stayone(str, s2[j]) == 0)
+			{
+				if (repeat(s2, s2[j], j) == 0)
+					write(1, &s2[j], 1);
+			}
 			j++;
 		}
-		i--;
-		while(k <= i)
-		{
-			if(check(str[k], str, k) == 1) 
-				write (1, &str[k], 1); 
-			k++;
-		}
 	}
-	write (1, "\n", 1);
+	write(1, "\n", 1);
+	return (0);
 }
