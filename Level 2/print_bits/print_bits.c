0df3a6d57487aab8ef48045dec908c787a219bc7
4trastos <93524280+4trastos@users.noreply.github.com>
2024-07-13 10:48:38 +0200

    Update print_bits.c

--- Level 2/print_bits/print_bits.c
+++ Level 2/print_bits/print_bits.c
@@ -1,13 +1,16 @@
 #include <unistd.h>
 
-void print_bits(unsigned char octet)
+void	print_bits(unsigned char octet)
 {
-	int	i = 8;
-	unsigned char 	bit;
+	int				i;
+	unsigned char	bit;
 
-	while (i--)
+	i = 7;
+	while (i >= 0)
 	{
-		bit = (octet >> i & 1) + '0';
+		bit = ((octet >> i) & 1) + '0';
 		write(1, &bit, 1);
+		i--;
 	}
+	write(1, "\n", 1);
 }
