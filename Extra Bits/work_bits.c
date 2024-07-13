24dfb7174a3507e9bc8c14a2086cb9809d074624
4trastos <93524280+4trastos@users.noreply.github.com>
2024-07-13 10:47:59 +0200

    Update work_bits.c

--- Extra Bits/work_bits.c
+++ Extra Bits/work_bits.c
@@ -56,12 +56,13 @@
 
 void	print_bits(unsigned char octet)
 {
-	int i = 8;
-	unsigned char bit;
+	int				i;
+	unsigned char	bit;
 
+	i = 7;
 	while (i >= 0)
 	{
-		bit = (octet >> i & 1) + '0';
+		bit = ((octet >> i) & 1) + '0';
 		write(1, &bit, 1);
 		i--;
 	}
